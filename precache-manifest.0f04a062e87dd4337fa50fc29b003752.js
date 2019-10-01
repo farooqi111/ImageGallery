e7f2e56326f5f3c3a6b86a2ac265bd855c02d62c
Muhammad Abdur Rehman Farooqi <38170390+farooqi111@users.noreply.github.com>
2019-10-01 17:48:32 +1000

    Updates

---
+++ precache-manifest.0f04a062e87dd4337fa50fc29b003752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97cc0c433ced71efbc",
+    "url": "/Image-Gallery/static/js/runtime~main.8c97cc0c.js"
+  },
+  {
+    "revision": "e85bad41510a78284425",
+    "url": "/Image-Gallery/static/js/main.e85bad41.chunk.js"
+  },
+  {
+    "revision": "02ba3405e7481ccc0a43",
+    "url": "/Image-Gallery/static/js/1.02ba3405.chunk.js"
+  },
+  {
+    "revision": "e85bad41510a78284425",
+    "url": "/Image-Gallery/static/css/main.cd8a4406.chunk.css"
+  },
+  {
+    "revision": "36ec588aabf6e43f60c0e5c0f8d26f73",
+    "url": "/Image-Gallery/index.html"
+  }
+];
